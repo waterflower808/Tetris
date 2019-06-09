cc3d82d3f30784588b4f258bba6b8c38de74d609
n13246 <48742459+n13246@users.noreply.github.com>
2019-06-09 21:30:58 +0900

    AI기능 정지

--- tetris_game.py
+++ tetris_game.py
@@ -9,7 +9,7 @@ from PyQt5.QtGui import QPainter, QColor
 from tetris_model import BOARD_DATA, Shape
 from tetris_ai import TETRIS_AI
 
-# TETRIS_AI = None
+TETRIS_AI = None
 
 class Tetris(QMainWindow):
     def __init__(self):
